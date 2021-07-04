a580b65d8b8a43080ad545de5d0d2d129736d7ae
Abdul <abdulrehman.shoukat@gmail.com>
2021-07-03 22:26:28 -0700

    complete oilerplate with schema and mutation

--- index.js
+++ index.js
@@ -3,14 +3,70 @@ const express = require('express')
 const app = express()
 const port = 3000
 const graphql = require('graphql')
+const {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList} = graphql
 // To create graphql server
 const { graphqlHTTP } = require('express-graphql')
 const mockdata= require("./MOCK_DATA.json")
 
+// Define each field type
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: ()=>({
+    id :{ type: GraphQLInt },
+    first_name :{ type: GraphQLString },
+    last_name :{ type: GraphQLString },
+    email :{ type: GraphQLString },
+    gender :{ type: GraphQLString },
+    ip_address :{ type: GraphQLString }
+  })
+})
+
+//fields are all queries so all endpoint in restapi
+// dont really need args but just for practise thats how you get args
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return mockdata;
+      },
+    },
+  },
+});
+
+
+const Mutation = new GraphQLObjectType({
+  name:"Mutations",
+  "fields":{
+    createUser: {
+      type: UserType,
+      args: {
+        first_name: { type: GraphQLString},
+        last_name: { type: GraphQLString},
+        email: { type: GraphQLString},
+        gender: { type: GraphQLString},
+        ip_address: { type: GraphQLString}
+      },
+      resolve(parent,args) {
+        //db.query('insert into') <--- if we were using postSQL
+        mockdata.push({id: mockdata.length +1, first_name: args.firstName, last_name: args.lastName, email: args.email, gender: args.gender, ip_address: args.ip_address })
+        return args
+      }
+    }
+  }
+})
+
+
+const schema = new GraphQLSchema({query:RootQuery , mutation:Mutation})
+
+
 app.use('/graphql',graphqlHTTP({
   schema,
-  graphql:true
+  graphiql:true
 }))
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
